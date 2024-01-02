31bc48e6a99da7289e8ca864fd4bf9fb65c7282f
azizHr <azizharkati20@gmail.com>
2024-01-02 13:58:14 +0100

    create article service

--- src/app/services/article/article.service.ts
+++ src/app/services/article/article.service.ts
@@ -1,33 +1,103 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { Article } from "../../models/article/article.model";
+import { ArticleResponse } from "../../models/article/article-response.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  public create(article: any): string {
-    return "create new article";
+  api: string = "http://localhost:8080/api/articles";
+
+  public create(article: Article): Observable<ArticleResponse> {
+    return this.http.post<ArticleResponse>(this.api, article)
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
   }
 
-  public update(article: any): string {
-    return "update an existing article";
+  public update(article: Article): Observable<ArticleResponse> {
+    return this.http.post<ArticleResponse>(this.api, article)
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
   }
 
-  public remove(id: string): string {
-    return "remove an existing article";
+  public remove(id: string): Observable<string> {
+    return this.http.delete<string>(`${this.api}/${id}`)
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
   }
 
-  public findById(id: string): string {
-    return "find a existing article by its id";
+  public findById(id: string): Observable<ArticleResponse> {
+    return this.http.get<ArticleResponse>(`${this.api}/${id}`)
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
   }
 
-  public findAll(): string {
-    return "find all articles";
+  public findAll(page: number, size: number): Observable<ArticleResponse[]> {
+    const params: HttpParams = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<ArticleResponse[]>(this.api, { params })
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
   }
 
-  public findAllByAuthor(author: string): string {
+  public searchByTitle(page: number, size: number, title: string): Observable<ArticleResponse[]> {
+    const params: HttpParams = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<ArticleResponse[]>(`${this.api}/search?title=${title}`, { params })
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
+  }
+
+  public searchByContent(page: number, size: number, content: string): Observable<ArticleResponse[]> {
+    const params: HttpParams = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<ArticleResponse[]>(`${this.api}/s?content=${content}`, { params })
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
+  }
+
+  public filterByTag(page: number, size: number, tag: string): Observable<ArticleResponse[]> {
+    const params: HttpParams = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<ArticleResponse[]>(`${this.api}/filter?tag=${tag}`, { params })
+        .pipe(
+            catchError((error: any) => {
+              console.log(error.error.message);
+              throw error;
+            })
+        );
+  }
+
+  public findAllByAuthor(page: number, size: number, author: string): string {
     return "find all articles by their author";
   }
 
